45563360337eac8326c1859356a25eed7787339b
Mahmoud Ayman <mahmoudk1000@gmail.com>
2025-09-19 23:36:19 +0300

    flake

--- flake.nix
+++ flake.nix
@@ -1,45 +1,68 @@
 {
   description = "Azure DevOps Runners Operator";
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs =
     { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        projectFile = "AzDORunner.csproj";
         dotnet-sdk = pkgs.dotnet-sdk_9;
         dotnet-runtime = pkgs.dotnet-runtime_9;
-        version = "0.1.0";
       in
       {
-        packages = {
+        packages = rec {
           default = pkgs.buildDotnetModule {
-            inherit
-              projectFile
-              dotnet-sdk
-              dotnet-runtime
-              version
-              ;
+            inherit dotnet-sdk dotnet-runtime;
+
             pname = "AzDORunner";
+            version = "0.1.0";
             src = ./.;
+            projectFile = "AzDORunner.csproj";
             nugetDeps = ./nix/deps.nix;
             doCheck = true;
             meta = with pkgs.lib; {
-              homepage = "github.com/mahmoudk1000/azdo-runner-operator";
+              description = "Azure DevOps Runners Operator";
+              homepage = "https://github.com/mahmoudk1000/azdo-runner-operator";
               license = licenses.mit;
               platforms = platforms.linux;
             };
           };
+
+          docker = pkgs.dockerTools.buildImage {
+            name = "mahmoudk1000/azdo-runner-operator";
+            tag = "latest";
+            contents = [ default ];
+            config = {
+              Cmd = [ "${default}/bin/AzDORunner" ];
+              User = "0:0";
+              WorkingDir = "/app";
+              ExposedPorts = {
+                "8080/tcp" = { };
+              };
+              Env = [
+                "ASPNETCORE_URLS=http://+:8080"
+                "DOTNET_RUNNING_IN_CONTAINER=true"
+              ];
+            };
+          };
         };
 
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = [ dotnet-sdk ] ++ (with pkgs; [ fantomas ]);
-          };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            dotnet-sdk
+          ];
+
+          shellHook = ''
+            export DOTNET_CLI_TELEMETRY_OPTOUT=1
+            export DOTNET_NOLOGO=1
+            export ASPNETCORE_ENVIRONMENT=Development
+          '';
         };
       }
     );
